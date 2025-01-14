998dbf53d18938dd73d28600afaa3dd30aa8a094
Gloria Sekyere <gloriasekyere15@gmail.com>
2025-01-13 17:38:01 -0800

    Remove tests from binary_search.py

--- scripts/binary_search.py
+++ scripts/binary_search.py
@@ -12,10 +12,3 @@ def binary_search(arr: list[int], num: int) -> int | None:
         elif guess < num:
             low = mid + 1
     return None
-
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-num = 9
-
-
-ans = binary_search(arr, num)
-print(ans)
