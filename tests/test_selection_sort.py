3bc033b902c68d5ac91961d1b0c22d81dd2eebce
Gloria Sekyere <gloriasekyere15@gmail.com>
2025-01-13 17:39:01 -0800

    Implement Selection Sort by sorting numbers from smallest to biggest

---
+++ tests/test_selection_sort.py
@@ -0,0 +1,19 @@
+from scripts.selection_sort import selection_sort
+
+
+def test_selection_sort():
+    arr = [3, 6, 8, 2, 1, 4, 7, 0, 9]
+    result = selection_sort(arr)
+    assert result == sorted(arr)
+
+
+def test_selection_sort_empty_array():
+    arr = []
+    result = selection_sort(arr)
+    assert result == []
+
+
+def test_selection_sort_already_sorted_array():
+    arr = [0, 1, 2, 3, 4, 5]
+    result = selection_sort(arr)
+    assert result == arr
