837d602dfbeb8cf8016ad762f03e475555d78e09
Gloria Sekyere <gloriasekyere15@gmail.com>
2025-02-02 18:35:28 -0800

    Implement Stack

---
+++ python/script/stack.py
@@ -0,0 +1,62 @@
+# stack implementation with python list
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, data):
+        self.stack.append(data)
+
+    def pop(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack.pop()
+
+    def peek(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.stack[-1]
+
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def size(self):
+        return len(self.stack)
+
+
+# stack implementation with singly linked list
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def push(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+        self.head = new_node
+        self.size += 1
+
+    def pop(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        removed_node = self.head
+        self.head = self.head.next
+        self.size -= 1
+        return removed_node.data
+
+    def peek(self):
+        if self.isEmpty():
+            return "Stack is empty"
+        return self.head.data
+
+    def isEmpty(self):
+        return self.head == None
+
+    def stack_size(self):
+        return self.size
