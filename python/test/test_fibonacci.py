5af1b6bbfdfff5220a7c01d53c74b63ae6498eb6
Gloria Sekyere <gloriasekyere15@gmail.com>
2025-02-08 17:52:07 -0800

    fibonacci script + tests

---
+++ python/test/test_fibonacci.py
@@ -0,0 +1,34 @@
+import pytest
+from script.fibonacci import fibonacci
+
+
+def test_fibonacci_zero():
+    assert fibonacci(0) == 0
+
+
+def test_fibonacci_one():
+    assert fibonacci(1) == 1
+
+
+def test_fibonacci_two():
+    assert fibonacci(2) == 1
+
+
+def test_fibonacci_three():
+    assert fibonacci(3) == 2
+
+
+def test_fibonacci_five():
+    assert fibonacci(5) == 5
+
+
+def test_fibonacci_ten():
+    assert fibonacci(10) == 55
+
+
+def test_fibonacci_negative():
+    assert fibonacci(-5) == 0
+
+
+def test_fibonacci_large():
+    assert fibonacci(20) == 6765
